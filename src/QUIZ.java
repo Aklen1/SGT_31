26050bcd141041cb4c4c7e0e341833035cc9fe52
Nellija Agapova <Nellija@gmail.com>
2022-07-27 21:46:53 +0300

    Arrays practice

---
+++ src/QUIZ.java
@@ -0,0 +1,44 @@
+public class QUIZ {
+    public static void main(String[] args) {
+        // Declare 2D array
+        // Fill array with values
+        //Print all values and format given
+
+
+        String [][] quizArray = new String[4][4];
+
+        quizArray[0][0] = "Capital city of Australia?";
+        quizArray[0][1] = "Sydney";
+        quizArray[0][2] = "Melbourne";
+        quizArray[0][3] = "Canberra";
+
+        quizArray[1][0] = "Capital city of USA?";
+        quizArray[1][1] = "Washington";
+        quizArray[1][2] = "New York";
+        quizArray[1][3] = "Dallas";
+
+        quizArray[2][0] = "Capital city of Brazil?";
+        quizArray[2][1] = "Rio de Janeiro";
+        quizArray[2][2] = "Brasilia";
+        quizArray[2][3] = "San paulo";
+
+        quizArray[3][0] = "Capital city of  Canada?";
+        quizArray[3][1] = "Montreal";
+        quizArray[3][2] = "Ottawa";
+        quizArray[3][3] = "Calgary";
+        System.out.println(quizArray[0][0]);
+        System.out.println(quizArray[0][3]);
+        System.out.println(quizArray[1][0]);
+        System.out.println(quizArray[1][1]);
+        System.out.println(quizArray[2][1]);
+        System.out.println(quizArray[2][2]);
+        System.out.println(quizArray[3][0]);
+        System.out.println(quizArray[3][2]);
+
+
+
+
+    }
+
+
+}
---
+++ src/homeWorks/Create_Array.java
@@ -0,0 +1,7 @@
+package homeWorks;
+
+public class Create_Array {
+    public static void main(String[] args) {
+        System.out.println("Enter the required size of the array");
+    }
+}
