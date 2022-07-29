49e6fd1f1486a6c2100407d52ec4ea8f367b97d4
Nellija Agapova <Nellija@gmail.com>
2022-07-29 21:22:36 +0300

    Loop Arrays Xmas tree practice

---
+++ src/Tree.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Tree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter any number!");
+        int input = scanner.nextInt();
+
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j < input - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
