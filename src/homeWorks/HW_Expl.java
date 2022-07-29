e92e8d50ebbcfe4844ed8a82001a27d9c3671b56
Nellija Agapova <Nellija@gmail.com>
2022-07-29 19:24:12 +0300

    Arrays Continue

---
+++ src/homeWorks/HW_Expl.java
@@ -0,0 +1,19 @@
+package homeWorks;
+
+import java.util.Arrays;
+
+public class HW_Expl {
+    public static void main(String[] args) {
+
+        int [] myArray = { 1,2,3};
+        int[] newArray = new int [myArray.length];
+
+        for (int i =0; i < myArray.length; i++) {
+            newArray[0] = myArray[0];
+
+        }
+            System.out.println("Source Array: " + Arrays.toString(myArray));
+            System.out.println("New Array: " + Arrays.toString(newArray));
+        }
+    }
+
